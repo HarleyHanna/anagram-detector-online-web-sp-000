04bbc5524771b74b1d0201bd4ff56dd897399256
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-27 02:49:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  
+  @empty_array = []
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.each do |array_word|
+      split_word = array_word.split("")
+      split_anagram = @word.split("")
+      if split_word.sort == split_anagram.sort
+        return array_word
+      elsif array.last == array_word && split_word.sort != split_anagram.sort
+        return @empty_array
+      end
+    end
+end
+
+end
