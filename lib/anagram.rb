75c91bd2e6805184fe2eaeedfb0e13ca7ec64fae
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-27 11:09:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,6 @@
 class Anagram
   
   @empty_array = []
-  @word_array = []
   
   attr_accessor :word
   
@@ -11,14 +10,15 @@ class Anagram
   
   def match(array)
     array.each do |array_word|
+      word_array = []
       split_word = array_word.split("")
       split_anagram = @word.split("")
       if split_word.sort == split_anagram.sort
-        @word_array << array_word
+        word_array << array_word
       end
       
     end
-    @word_array
+    
 end
 
 end
